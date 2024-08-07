48d7c3bffa1738741940aefd41a2871ca9d98d81
何东亮 <beyondsoft-290-hedongliang01@beyondsoft.com>
2024-08-07 17:12:29 +0800

    feat: 区分环境

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,24 @@
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { defineConfig, loadEnv } from "vite";
-import eslintPlugin from "vite-plugin-eslint";
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+import { defineConfig, loadEnv } from 'vite';
+import eslintPlugin from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
+
   return {
     plugins: [
       react(),
       eslintPlugin({
         cache: false, // 关闭eslint缓存，在开发过程中更实时的反馈
-        include: ["src/**/*.ts", "src/**/*.tsx", "src/**/*.js", "src/**/*.jsx"], // 包含的文件
-        exclude: ["node_modules"], // 排除的文件
+        include: ['src/**/*.ts', 'src/**/*.tsx', 'src/**/*.js', 'src/**/*.jsx'], // 包含的文件
+        exclude: ['node_modules'], // 排除的文件
       }),
     ],
     resolve: {
       alias: {
-        "@": path.resolve(__dirname, "./src"),
+        '@': path.resolve(__dirname, './src'),
       },
     },
     css: {
@@ -30,15 +31,16 @@ export default defineConfig(({ mode }) => {
       },
     },
     server: {
-      host: "0.0.0.0",
+      host: '0.0.0.0',
       port: 9999,
       // 代理
       proxy: {
         [env.VITE_BASE_API]: {
-          target: "http://localhost:8989",
+          target: env.VITE_HOST,
           changeOrigin: true,
           disableHostCheck: true,
-          rewrite: (path) => env.VITE_ENV === 'development' ? path.replace(/^\/dev-api/, '') : path.replace(/^\/prod-api/, '')
+          rewrite: (path) =>
+            env.VITE_ENV === 'development' ? path.replace(/^\/dev-api/, '') : path.replace(/^\/prod-api/, ''),
         },
       },
     },
