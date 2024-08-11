8e024ec5da974bd42bf8eaa1ef6e8cfb6802b351
何东亮 <beyondsoft-290-hedongliang01@beyondsoft.com>
2024-08-11 10:06:38 +0800

    feat: 修复跳转问题

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,12 +1,12 @@
 ﻿import { store } from '@/store';
-import { LogOut } from "@/store/reducerModule/userReducer";
-import { getToken, removeToken } from "@/utils/auth";
-import errorCode from "@/utils/errorCode";
-import { message } from "antd";
+import { LogOut } from '@/store/reducerModule/userReducer';
+import { getToken, removeToken } from '@/utils/auth';
+import errorCode from '@/utils/errorCode';
+import { message } from 'antd';
 
-import axios from "axios";
+import axios from 'axios';
 
-axios.defaults.headers["Content-Type"] = "application/json;charset=utf-8";
+axios.defaults.headers['Content-Type'] = 'application/json;charset=utf-8';
 // 创建axios实例
 const service = axios.create({
   // axios中请求配置有baseURL选项，表示请求URL公共部分
@@ -20,25 +20,25 @@ service.interceptors.request.use(
     // 是否需要设置 token
     const isToken = (config.headers || {}).isToken === false;
     if (getToken() && !isToken) {
-      config.headers["token"] = getToken(); // 让每个请求携带自定义token 请根据实际情况自行修改
+      config.headers['token'] = getToken(); // 让每个请求携带自定义token 请根据实际情况自行修改
     }
     // get请求映射params参数
-    if (config.method === "get" && config.params) {
-      let url = config.url + "?";
+    if (config.method === 'get' && config.params) {
+      let url = config.url + '?';
       for (const propName of Object.keys(config.params)) {
         const value = config.params[propName];
-        const part = encodeURIComponent(propName) + "=";
-        if (value !== null && typeof value !== "undefined") {
-          if (typeof value === "object") {
+        const part = encodeURIComponent(propName) + '=';
+        if (value !== null && typeof value !== 'undefined') {
+          if (typeof value === 'object') {
             for (const key of Object.keys(value)) {
-              if (value[key] !== null && typeof value[key] !== "undefined") {
-                const params = propName + "[" + key + "]";
-                const subPart = encodeURIComponent(params) + "=";
-                url += subPart + encodeURIComponent(value[key]) + "&";
+              if (value[key] !== null && typeof value[key] !== 'undefined') {
+                const params = propName + '[' + key + ']';
+                const subPart = encodeURIComponent(params) + '=';
+                url += subPart + encodeURIComponent(value[key]) + '&';
               }
             }
           } else {
-            url += part + encodeURIComponent(value) + "&";
+            url += part + encodeURIComponent(value) + '&';
           }
         }
       }
@@ -59,7 +59,7 @@ service.interceptors.response.use(
     // 未设置状态码则默认成功状态
     const code = res.data.code || 200;
     // 获取错误信息
-    const msg = errorCode[code] || res.data.msg || errorCode["default"];
+    const msg = errorCode[code] || res.data.msg || errorCode['default'];
     if (code === 401) {
       message.error(msg);
       // const res = await logout(getToken())
@@ -69,31 +69,31 @@ service.interceptors.response.use(
       //   location.href = "/login";
       // }
       removeToken();
-      store.dispatch(LogOut)
-      location.href = "/login";
-      return {code: 401, msg: msg, data: null};
+      store.dispatch(LogOut);
+      location.href = '/#/login';
+      return { code: 401, msg: msg, data: null };
     } else if (code === 500) {
       message.error(msg);
-      return {code: 500, msg: msg, data: null};
+      return { code: 500, msg: msg, data: null };
     } else if (code !== 200) {
       message.error(msg);
-      return {code: code, msg: msg, data: null};
+      return { code: code, msg: msg, data: null };
     } else {
       //把字符串total 转换成 数字 total
       if (res.data.data && res.data.data.total) {
         res.data.data.total = parseInt(res.data.data.total);
       }
-      return {code: 200, msg: msg, data: res.data.data};
+      return { code: 200, msg: msg, data: res.data.data };
     }
   },
   (error) => {
     let { message: msg } = error;
-    if (msg === "Network Error") {
-      msg = "后端接口连接异常";
-    } else if (msg.includes("timeout")) {
-      msg = "系统接口请求超时";
-    } else if (msg.includes("Request failed with status code")) {
-      msg = "系统接口" + msg.substr(msg.length - 3) + "异常";
+    if (msg === 'Network Error') {
+      msg = '后端接口连接异常';
+    } else if (msg.includes('timeout')) {
+      msg = '系统接口请求超时';
+    } else if (msg.includes('Request failed with status code')) {
+      msg = '系统接口' + msg.substr(msg.length - 3) + '异常';
     }
     message.error(msg);
     return Promise.reject(error);
